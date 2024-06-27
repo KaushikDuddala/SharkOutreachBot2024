7f36476fbbb55b2cd9fd75a333608f6cad619f4a
KaushikDuddala <kaushik.duddala@gmail.com>
2024-06-27 09:12:39 -0500

    other funcs besides tracking pos setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/turrent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/turrent.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 
@@ -11,6 +12,13 @@ public class turrent {
 
     private int stage = 0;
     private int fullRots = 0;
+    private double targetPos = 0.00;
+
+    private double technicalPos = axonPos;
+
+    private double kP = 0.2, kI = 0, kD = 0;
+
+    private PIDController pid = new PIDController(kP, kI, kD);
 
     public turrent(CRServo axonServo, AnalogInput axonPosInput)
     {
@@ -20,9 +28,26 @@ public class turrent {
 
     public void trackPos()
     {
+        axonPos = axonPosInput.getVoltage() / 3.3;
         if(stage == 0) // last reported under 0.5 position
         {
 
         }
+    }
+
+    public void calc()
+    {
+        double power = pid.calculate(technicalPos, targetPos);
+        axonServo.setPower(power);
+    }
+
+    public double getPos()
+    {
+        return technicalPos;
+    }
+
+    public void setTarget(double target)
+    {
+        target = target;
     }
 }
